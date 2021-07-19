23a13aa0f14121c622bb605eec31d380ea544dac
AlfaroJoel <alfarojoelf@gmail.com>
2021-07-19 03:14:37 -0300

    style: add animation button

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,19 +1,38 @@
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
 
 import CartIcon from '../Cart/CartIcon';
 import CartContext from '../../store/CartContext';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
-
+    const [btnIsHighlighted, setBtnIsHighlight] = useState(false);
     const cartCtx = useContext(CartContext)
 
-    const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+    const {items} = cartCtx;
+
+    useEffect(() => {
+        if(items.length === 0){
+            return;
+        }
+        setBtnIsHighlight(true);
+
+        const timer = setTimeout(() => {
+            setBtnIsHighlight(false);
+        }, 300)
+
+        return () => {
+            clearTimeout(timer)
+        }
+    }, [items])
+
+    const numberOfCartItems = items.reduce((curNumber, item) => {
         return curNumber + item.amount;
     }, 0)
 
     return (
-        <button className={classes.button} onClick={props.onClick}>
+        <button className={btnClasses} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
